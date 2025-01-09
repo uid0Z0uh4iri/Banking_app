01d5eea89dc0c1baff6d7a59bc51806399a652ec
Z0UH4IRI <mohamedzouhairi023@gmail.com>
2025-01-09 14:59:28 +0100

    Configuration of SMTP Of Gmail

--- classes/Mailer.php
+++ classes/Mailer.php
@@ -21,18 +21,18 @@ $mail = new PHPMailer(true);
     //Server settings
     // $mail->SMTPDebug = SMTP::DEBUG_SERVER;                      //Enable verbose debug output
     $mail->isSMTP();                                            //Send using SMTP
-    $mail->Host       = 'sandbox.smtp.mailtrap.io';                     //Set the SMTP server to send through
+    $mail->Host       = 'smtp.gmail.com';                     //Set the SMTP server to send through
     $mail->SMTPAuth   = true;                                   //Enable SMTP authentication
-    $mail->Username   = '323f68d97c5025';                     //SMTP username
-    $mail->Password   = 'f381bf8f68279d';                               //SMTP password
+    $mail->Username   = 'zouhairihlima@gmail.com';                     //SMTP username
+    $mail->Password   = 'yydk njrk zrzl vzow';                               //SMTP password
     $mail->SMTPSecure = "SSL";            //Enable implicit TLS encryption
-    $mail->Port       = 465;                                    //TCP port to connect to; use 587 if you have set `SMTPSecure = PHPMailer::ENCRYPTION_STARTTLS`
+    $mail->Port       = 587;                                    //TCP port to connect to; use 587 if you have set `SMTPSecure = PHPMailer::ENCRYPTION_STARTTLS`
 
     $mail->Subject = 'Here is the subject';
-    $mail->setFrom('itsmezouhairi@gmail.com', 'Mailer');
+    $mail->setFrom('zouhairihlima@gmail.com', 'Banka2Ka');
     $mail->AltBody = 'This is the body in plain text for non-HTML mail clients';
     $mail->addAddress($email, $name);
-    $mail->Body    = 'Welcome to Baka2Ka, this is your password you should update it and update your informations in the app. Password : <b>'. $password .' </b>';
+    $mail->Body    = ' Welcome to Baka2Ka, this is your password you should update it and update your informations in the app. Password : <b>'. $password .' </b>';
     $mail->send();
     return true;
 }
